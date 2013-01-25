b04693e1424c1b0e884d5dfaae09dc3d12d9308b
Shiqiao Du <lucidfrontier.45@gmail.com>
2013-01-25 22:13:43 +0900

    modified flags of face detector

--- pyface/detector.py
+++ pyface/detector.py
@@ -12,7 +12,10 @@ from .utils import toGray
 #haar_detector = cv2.CascadeClassifier("data/haarcascades/haarcascade_frontalface_default.xml")
 #lbp_detector = cv2.CascadeClassifier("data/lbpcascades/lbpcascade_frontalface.xml")
 
-def detectFaces(gray_img, detector, flags=cv.CV_HAAR_SCALE_IMAGE, min_size=(64, 64)):
+BIGGEST_FACE = cv.CV_HAAR_FIND_BIGGEST_OBJECT | cv.CV_HAAR_DO_ROUGH_SEARCH
+MANY_FACES = cv.CV_HAAR_SCALE_IMAGE
+
+def detectFaces(gray_img, detector, flags=MANY_FACES, min_size=(64, 64)):
     rects = detector.detectMultiScale(gray_img, scaleFactor=1.1, minNeighbors=3,
             minSize=min_size, flags=flags)
     faces = []
@@ -22,7 +25,7 @@ def detectFaces(gray_img, detector, flags=cv.CV_HAAR_SCALE_IMAGE, min_size=(64, 
         faces.append(face)
     return faces
 
-def detectOneFace(gray_img, detector, flags=cv.CV_HAAR_SCALE_IMAGE, 
+def detectOneFace(gray_img, detector, flags=BIGGEST_FACE, 
                   min_size=(64, 64), resize="min_size"):
     
     roi = detectOneFaceROI(gray_img, detector, flags, min_size)
@@ -38,37 +41,39 @@ def detectOneFace(gray_img, detector, flags=cv.CV_HAAR_SCALE_IMAGE,
         pass
     return face
 
-def detectOneFaceROI(gray_img, detector, flags=cv.CV_HAAR_SCALE_IMAGE, 
-                  min_size=(64, 64)):
+def detectOneFaceROI(gray_img, detector, flags=BIGGEST_FACE,
+                      min_size=(64, 64)):
     rects = detector.detectMultiScale(gray_img, scaleFactor=1.1, minNeighbors=3,
             minSize=min_size, flags=flags)
     if len(rects) == 0:
         return None
     
-    sizes = [w * h for x, y, w, h in rects]
-    largest_idx = np.argmax(sizes)
-    x, y, w, h = rects[largest_idx]
+#    sizes = [w * h for x, y, w, h in rects]
+#    largest_idx = np.argmax(sizes)
+#    x, y, w, h = rects[largest_idx]
+    
+    x, y, w, h = rects[0]
+    
     return (x, y, w, h)
 
 class FaceDetector(object):
-    def __init__(self, detector, flags=cv.CV_HAAR_SCALE_IMAGE, min_size=(64, 64)):
+    def __init__(self, detector, min_size=(64, 64)):
         if isinstance(detector, str):
             self.detector_ = cv2.CascadeClassifier(detector)
         else:    
             self.detector_ = detector
-        self.flags_ = flags
         self.min_size_ = min_size
         
     def detectFaces(self, gray_img):
-        return detectFaces(gray_img, self.detector_, self.flags_,
+        return detectFaces(gray_img, self.detector_, MANY_FACES,
                             self.min_size_)
 
     def detectOneFace(self, gray_img, resize="min_size"):
-        return detectOneFace(gray_img, self.detector_, self.flags_,
+        return detectOneFace(gray_img, self.detector_, BIGGEST_FACE,
                               self.min_size_, resize=resize)
         
     def detectOneFaceROI(self, gray_img):
-        return detectOneFaceROI(gray_img, self.detector_, self.flags_,
+        return detectOneFaceROI(gray_img, self.detector_, BIGGEST_FACE,
                             self.min_size_)
 
 if __name__ == "__main__":
